9f5236907d2f22c2dd4ee54cf60701f790feaa2d
Brian Cajulis <brianrobertcajulis@yahoo.com>
2020-02-29 10:58:07 -0800

    created transaction route functionality

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors'); // adds color to console (not needed)
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
 const app = express();
+
+app.use(express.json()); // enables us to us the body parser
 
 app.use('/api/v1/transactions', transactions);
 
