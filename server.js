811c65510ca13f3fe24de70fe0c8b1612e7c0b95
Brian Robert Cajulis <brianrobertcajulis@yahoo.com>
2020-02-28 16:21:56 -0800

    add transaction routes

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const dotenv = require('dotenv');
 const colors = require('colors'); // adds color to console (not needed)
 const morgan = require('morgan');
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+const transactions = require('./routes/transactions');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
